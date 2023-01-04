e01e91eef390d6318453b06c58a84bafe94196ba
Lincon Cardoso <59518299+lincon-cardoso@users.noreply.github.com>
2023-01-04 15:30:57 -0300

    inicio React

---
+++ organo/src/App.js
@@ -0,0 +1,16 @@
+
+import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto/CampoTexto';
+
+
+function App() {
+  return (
+    <div className="App">
+       <Banner/>
+        <CampoTexto/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal Pagina" />
+        </header>
+    )
+}
+
+export default Banner;
