304c44b46ec1e13c03f57de0e45f77d23886fe5e
Lincon Cardoso <59518299+lincon-cardoso@users.noreply.github.com>
2023-01-04 19:55:18 -0300

    atualização

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,14 +1,15 @@
 
 import Banner from './componentes/Banner/Banner';
-import CampoTexto from './componentes/CampoTexto/CampoTexto';
+import Formulario from './componentes/Formulario/Formulario';
+
 
 
 function App() {
   return (
     <div className="App">
        <Banner/>
-        <CampoTexto/>
-
+       <Formulario/>
+       
     </div>
   );
 }
--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -1,10 +1,11 @@
 import './campoTexto.css';
 
-const CampoTexto = ()=>{
+const CampoTexto = (props)=>{
+    // console.log(props.label)
     return(
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder='Digite o seu nome'/>
+            <label>{props.label}</label>
+            <input placeholder={props.placeholder}/>
         </div>
     )
 
